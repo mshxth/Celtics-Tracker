279ea4be353e224b281f03090436d4b690fc5a65
Manav Sheth <122500006+mshxth@users.noreply.github.com>
2023-07-25 23:26:26 -0400

    Update celtics.js

--- javascript/celtics.js
+++ javascript/celtics.js
@@ -145,16 +145,6 @@ function getLastName(name) {
 	}
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-	const rows = document.querySelectorAll("tr[data-href]");
-
-	rows.forEach(row => {
-		row.addEventListener("click", () => {
-			window.location.href = row.dataset.href;
-		});
-	});
-});
-
 
 
 
